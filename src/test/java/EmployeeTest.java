2e20e897012e52dd7f435630aa9d122a011216d5
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-03 13:45:42 +0100

    employee has name

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EmployeeTest {
+
+    Employee employee;
+
+    @Before
+    public void before() {
+        employee = new Pilot("Jim", Rank.CAPTAIN, "AB1234");
+    }
+
+    @Test
+    public void employeeHasName() {
+        assertEquals("Jim", employee.getName());
+    }
 }
