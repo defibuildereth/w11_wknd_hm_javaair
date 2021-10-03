f1319bc4e6e5bfc2ae1ab6ea965445882b31c678
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-03 13:49:11 +0100

    pilot has license number

--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before() {
+        pilot = new Pilot("Jim", Rank.CAPTAIN, "AB1234");
+    }
+
+    @Test
+    public void pilotHasLicenseNum() {
+        assertEquals("AB1234", pilot.getLicenseNum());
+    }
 }
