6df3c729bb3c2379df5dc6a93f97af1061dd71fe
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-03 13:52:38 +0100

    cabinCrewMember has rank

--- src/test/java/CabinCrewMemberTest.java
+++ src/test/java/CabinCrewMemberTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewMemberTest {
+
+    CabinCrewMember cabinCrewMember;
+
+    @Before
+    public void before() {
+        cabinCrewMember = new CabinCrewMember("Alice", Rank.FLIGHTATTENDANT);
+    }
+
+    @Test
+    public void cabinCrewMemberHasRank() {
+        assertEquals(Rank.FLIGHTATTENDANT, cabinCrewMember.getRank());
+    }
 }
