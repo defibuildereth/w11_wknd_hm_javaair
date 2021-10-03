fb0144351259634f1411a40bdaa9285aae0ce914
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-03 14:17:34 +0100

    flight has pilot

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
-}
+
+    Flight flight;
+    Pilot pilot;
+    CabinCrewMember cabinCrewMember;
+    Passenger passenger;
+    Plane plane;
+
+
+    @Before
+    public void before() {
+        pilot = new Pilot("Jim", Rank.CAPTAIN, "AB1234");
+        cabinCrewMember = new CabinCrewMember("Alice", Rank.FLIGHTATTENDANT);
+        passenger = new Passenger("Donald", 3);
+        plane = Plane.AIRBUS420;
+        ArrayList<Passenger> passengers = new ArrayList<>();
+        passengers.add(passenger);
+        ArrayList<CabinCrewMember> cabinCrew = new ArrayList<>();
+        cabinCrew.add(cabinCrewMember);
+        flight = new Flight(pilot, cabinCrew, passengers, plane, "EZ123", "Edinburgh", "Vienna", "20:00");
+
+    }
+    @Test
+    public void flightHasPilot() {
+        assertEquals("Jim", flight.getPilot().getName());
+        }
+    }
+
